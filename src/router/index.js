33e11d68efce0550412caad6e65dff6584ccbebf
xiatec <1870812219@qq.com>
2021-10-26 00:27:49 +0800

    登录页面代码拆分及弹窗效果实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/home/Home'
 import Login from '../views/login/Login'
+import Register from "../views/register/Register"
 
 const routes = [{
     path: '/',
     name: 'Home',
     component: Home
-  }, {
+}, {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    beforeEnter: (to, from, next) => {
+      const isLogin = localStorage.isLogin;
+      isLogin ? next({ name: "Home" }) : next();
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    beforeEnter: (to, from, next) => {
+      const isLogin = localStorage.isLogin;
+      isLogin ? next({ name: "Home" }) : next();
+    }
   },
   // {
   //   path: '/about',
@@ -24,6 +38,12 @@ const routes = [{
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.isLogin;
+  const name = to.name;
+  const isLoginOrRegister = (name === "Login" || name === "Register");
+  (isLogin || isLoginOrRegister) ? next() : next({ name: "Login"})
 })
 
 export default router
