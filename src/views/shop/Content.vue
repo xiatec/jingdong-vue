d7df4600f4367b9fbab0cf2ae8e8c9e21f4718b4
xiatec <1870812219@qq.com>
2021-11-01 23:54:31 +0800

    购物车功能完善,实现数据联动,全选,取消功能

--- src/views/shop/Content.vue
+++ src/views/shop/Content.vue
@@ -2,36 +2,39 @@
   <div class="content">
     <div class="category">
       <div
-        :class="{
-          category__item: true,
-          'category__item--active': currentTab === item.tab
-        }"
+        :class="{'category__item': true, 'category__item--active': currentTab === item.tab}"
         v-for="item in categories"
         :key="item.name"
-        @click="() => handleCategoryClick(item.tab)"
+        @click="() => handleTabClick(item.tab)"
       >
-        {{ item.name }}
+        {{item.name}}
       </div>
     </div>
     <div class="product">
-      <div class="product__item" v-for="item in contentList" :key="item._id">
-        <img
-          src="http://www.dell-lee.com/imgs/vue3/near.png"
-          alt="img"
-          class="product__item__img"
-        />
+      <div
+        class="product__item"
+        v-for="item in list"
+        :key="item._id"
+      >
+        <img class="product__item__img" :src="item.imgUrl" />
         <div class="product__item__detail">
-          <h4 class="product__item__title">{{ item.name }}</h4>
-          <p class="product__item__sales">月售{{ item.sales }}件</p>
+          <h4 class="product__item__title">{{item.name}}</h4>
+          <p class="product__item__sales">月售 {{item.sales}} 件</p>
           <p class="product__item__price">
-            <span class="product__item__yen">&yen;</span>{{ item.price }}
-            <span class="product__item__origin">&yen;{{ item.oldPrice }}</span>
+            <span class="product__item__yen">&yen;</span>{{item.price}}
+            <span class="product__item__origin">&yen;{{item.oldPrice}}</span>
           </p>
         </div>
         <div class="product__number">
-          <span class="product__number__minus">-</span>
-          10
-          <span class="product__number__plus">+</span>
+          <span
+            class="product__number__minus"
+            @click="() => { changeCartItemInfo(shopId, item._id, item, -1) }"
+          >-</span>
+            {{item.count || 0}}
+          <span
+            class="product__number__plus"
+            @click="() => { changeCartItemInfo(shopId, item._id, item, 1) }"
+          >+</span>
         </div>
       </div>
     </div>
@@ -39,62 +42,78 @@
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
-import { get } from "../../utils/request";
-export default {
-  name: "Content",
-  setup() {
-    const categories = [
-      { name: "全部商品", tab: "all" },
-      { name: "秒杀", tab: "seckill" },
-      { name: "新鲜水果", tab: "fruit" }
-    ];
-    const data = reactive({
-      currentTab: categories[0].tab,
-      contentList: []
-    });
-    const getContentData = async tab => {
-      const result = await get("/api/shop/1/products", { tab });
-      if (result?.errno === 0 && result?.data?.length) {
-        data.contentList = result.data;
-      }
-    };
-    // 需要传参数时, 绑定的事件函数建议使用箭头函数的形式
-    const handleCategoryClick = tab => {
-      getContentData(tab);
-      data.currentTab = tab;
-    };
-    getContentData("all");
-    const { contentList, currentTab } = toRefs(data);
-    return {
-      categories,
-      currentTab,
-      contentList,
-      handleCategoryClick
-    };
+import { reactive, ref, toRefs, watchEffect } from 'vue'
+import { useRoute } from 'vue-router'
+import { get } from '../../utils/request'
+import { useCommonCartEffect } from './commonCartEffect'
+
+const categories = [
+  { name: '全部商品', tab: 'all' },
+  { name:'秒杀', tab: 'seckill' },
+  { name: '新鲜水果', tab: 'fruit'}
+]
+
+// Tab 切换相关的逻辑
+const useTabEffect = () => {
+  const currentTab = ref(categories[0].tab)
+  const handleTabClick = (tab) => {
+    currentTab.value = tab
   }
-};
+  return { currentTab, handleTabClick}
+}
+
+// 列表内容相关的逻辑
+const useCurrentListEffect = (currentTab, shopId) => {
+  const content = reactive({ list: [] })
+  const getContentData = async () => {
+    const result = await get(`/api/shop/${shopId}/products`, {
+      tab: currentTab.value
+    })
+    if(result?.errno === 0 && result?.data?.length) {
+      content.list = result.data;
+    }
+  }
+  watchEffect(() => { getContentData() })
+  const { list } = toRefs(content)
+  return { list }
+}
+
+export default {
+  name: 'Content',
+  setup() {
+    const route = useRoute()
+    const shopId = route.params.id
+    const { currentTab, handleTabClick } = useTabEffect()
+    const { list } = useCurrentListEffect(currentTab, shopId)
+    const { changeCartItemInfo } = useCommonCartEffect()
+    return {
+      categories, currentTab, handleTabClick, list,
+      shopId, changeCartItemInfo,
+    }
+  }
+}
 </script>
+
 <style lang="scss" scoped>
-@import "../../style/viriables.scss";
-@import "../../style/mixins.scss";
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
 .content {
   display: flex;
   position: absolute;
   left: 0;
   right: 0;
   top: 1.5rem;
-  bottom: 0.5rem;
+  bottom: .5rem;
 }
 .category {
   overflow-y: scroll;
   height: 100%;
-  width: 0.76rem;
+  width: .76rem;
   background: $search-bgColor;
   &__item {
-    line-height: 0.4rem;
+    line-height: .4rem;
     text-align: center;
-    font-size: 0.14rem;
+    font-size: 14px;
     color: #333;
     &--active {
       background: $bgColor;
@@ -107,70 +126,71 @@ export default {
   &__item {
     position: relative;
     display: flex;
-    padding: 0.12rem 0;
-    margin: 0 0.16rem;
-    border-bottom: 0.01rem solid $content-bgColor;
+    padding: .12rem 0;
+    margin: 0 .16rem;
+    border-bottom: .01rem solid $content-bgColor;
     &__detail {
       overflow: hidden;
     }
     &__img {
-      width: 0.68rem;
-      height: 0.68rem;
-      margin-right: 0.16rem;
+      width: .68rem;
+      height: .68rem;
+      margin-right: .16rem;
     }
     &__title {
       margin: 0;
-      line-height: 0.2rem;
-      font-size: 0.14rem;
+      line-height: .2rem;
+      font-size: .14rem;
       color: $content-fontcolor;
       @include ellipsis;
     }
     &__sales {
-      margin: 0.06rem 0;
-      font-size: 0.12rem;
+      margin: .06rem 0;
+      font-size: .12rem;
       color: $content-fontcolor;
     }
     &__price {
       margin: 0;
-      line-height: 0.2rem;
-      font-size: 0.14rem;
-      color: #e93b3b;
+      line-height: .2rem;
+      font-size: .14rem;
+      color: $hightlight-fontColor;
     }
     &__yen {
-      font-size: 0.12rem;
+      font-size: .12rem;
     }
     &__origin {
-      margin-left: 0.06rem;
-      line-height: 0.2rem;
-      font-size: 0.12rem;
-      color: #999;
+      margin-left: .06rem;
+      line-height: .2rem;
+      font-size: .12rem;
+      color: $light-fontColor;
       text-decoration: line-through;
     }
+    .product__number {
+      position: absolute;
+      right: 0;
+      bottom: .12rem;
+      &__minus, &__plus
+       {
+        display: inline-block;
+        width: .2rem;
+        height: .2rem;
+        line-height: .16rem;;
+        border-radius: 50%;
+        font-size: .2rem;
+        text-align: center;
+      }
+      &__minus {
+        border: .01rem solid $medium-fontColor;
+        color: $medium-fontColor;
+        margin-right: .05rem;
+      }
+      &__plus {
+        background: $btn-bgColor;
+        color: $bgColor;
+        margin-left: .05rem;
+      }
+    }
   }
-  &__number {
-    position: absolute;
-    right: 0;
-    bottom: 0.12rem;
-    &__minus,
-    &__plus {
-      display: inline-block;
-      width: 0.2rem;
-      height: 0.2rem;
-      line-height: 0.16rem;
-      border-radius: 50%;
-      font-size: 0.2rem;
-      text-align: center;
-    }
-    &__minus {
-      border: 0.01rem solid $medium-fontColor;
-      color: $medium-fontColor;
-      margin-right: 0.05rem;
-    }
-    &__plus {
-      background: $btn-bgColor;
-      color: $bgColor;
-      margin-left: 0.05rem;
-    }
-  }
+  
 }
 </style>
